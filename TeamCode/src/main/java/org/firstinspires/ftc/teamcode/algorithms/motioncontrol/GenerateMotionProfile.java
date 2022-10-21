e15abd122d20b4d4304ee62d59c5a1c7e459bab7
jovanus <eric.weber.r@gmail.com>
2022-10-21 00:10:23 -0400

    Improve Motion  P

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/algorithms/motioncontrol/GenerateMotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/algorithms/motioncontrol/GenerateMotionProfile.java
@@ -1,92 +1,45 @@
 package org.firstinspires.ftc.teamcode.algorithms.motioncontrol;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class GenerateMotionProfile {
-    ProfilePoint curPoint;
-    double setPoint;
-    double maxV;
-    double maxA;
-    double dt;
 
-    public GenerateMotionProfile(double setPoint, double maxV, double maxA, double dt) {
-        this.curPoint = new ProfilePoint(0,0,0,0);
-        this.setPoint = setPoint;
-        this.maxV = maxV;
-        this.maxA = maxA;
-        this.dt = dt;
-    }
-    public GenerateMotionProfile(ProfilePoint curPoint, double setPoint, double maxV, double maxA, double dt) {
-        this.curPoint = curPoint;
-        this.setPoint = setPoint;
-        this.maxV = maxV;
-        this.maxA = maxA;
-        this.dt = dt;
-    }
+    public static Queue<ProfilePoint> acceleration(ProfilePoint LastPoint, double vel, double accel, double dt){
 
-    public ProfilePoint[] generate(){
+        Queue<ProfilePoint> output = new LinkedList<>();
 
+        for (double t = LastPoint.getTime(); t < timetoaccelerate(LastPoint,vel,accel) ; t=+dt) {
 
-        ProfilePoint[] output = new ProfilePoint[requiredPoints()];
-        // Generate first point
-        output[0] = curPoint;
-        int i = 1;
-        // Acceleration
-        for (; ; i++) {
-            output[i] = new ProfilePoint(
-                    dt + output[i-1].getTime(),
-                    output[i-1].getPosition() + dt * output[i-1].getVelocity(),
-                    output[i-1].getVelocity() + dt * maxA,
-                    maxA);
-            if (output[i].getVelocity() + dt * maxA - maxV > 0) break;
+            output.add( new ProfilePoint(
+                    t,
+                    LastPoint.getPosition() + LastPoint.getVelocity()*(t-LastPoint.getTime()) + 0.5 * accel * Math.pow(t-LastPoint.getTime(),2),
+                    (vel - LastPoint.getVelocity() > 0 ? 1 : -1) * accel * (t-LastPoint.getTime()),
+                    accel));
         }
-        // Cruise
-        for (;i < endOfCruise() ; i++) {
-            output[i] = new ProfilePoint(
-                    dt + output[i-1].getTime(),
-                    output[i-1].getPosition() + dt * output[i-1].getVelocity(),
-                    maxV,
-                    0);
-        }
-
-        // Decceleration
-        for (; ; i++) {
-            output[i] = new ProfilePoint(
-                    output[i-1].getTime() + dt,
-                    output[i-1].getPosition() + dt * output[i-1].getVelocity(),
-                    output[i-1].getVelocity() + dt * maxA,
-                    -maxA);
-            if (output[i].getVelocity() - dt * maxA > 0) break;
-        }
-        output[i+1] = new ProfilePoint(output[i].getTime() + dt,
-                output[i].getPosition() + dt * output[i].getVelocity(),
-                0,
-                0);
 
         return output;
-
     }
 
-    double distancetoaccelerate(){
-        return curPoint.getVelocity() * timetoaccelerate() + 1 / 2 * (maxV - curPoint.getVelocity()) * timetoaccelerate();
+    public static Queue<ProfilePoint> cruse(ProfilePoint LastPoint, double time, double dt){
+
+        Queue<ProfilePoint> output = new LinkedList<>();
+
+        for (double t = LastPoint.getTime(); t < time - LastPoint.getTime() ; t=+dt) {
+
+            output.add(new ProfilePoint(
+                    t,
+                    LastPoint.getPosition() + LastPoint.getVelocity()*(t-LastPoint.getTime()),
+                    LastPoint.getVelocity(),
+                    0));
+        }
+
+        return output;
     }
 
-    double distancetoDecelerate(){
-        return maxV * timetoDecelerate() * 1/ 2;
+
+    static double timetoaccelerate(ProfilePoint curPoint, double vel, double accel){
+        return Math.abs((vel - curPoint.getVelocity())/accel);
     }
 
-    double timetoaccelerate(){
-        return Math.abs((maxV - curPoint.getVelocity())/maxA)
-                ;
-    }
-
-    double timetoDecelerate() {
-        return maxV/maxA;
-    }
-
-    double totalTime(){
-
-    }
-
-    double endOfCruise(){
-        return
-    }
 }
