3f44eb75724189626c9ff249a868fff89d0c8175
Greg Weaver <greg.weaver@careevolution.com>
2022-10-22 14:48:16 -0400

    teleop working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.hardware.rev.RevTouchSensor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.*;
 
 import org.firstinspires.ftc.teamcode.Subsystems.*;
 import org.firstinspires.ftc.teamcode.algorithms.*;
@@ -37,18 +37,11 @@ public class Teleop extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-
-            //mechanum.calcMove(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_y);
-            // mechanum.M[0-3] has how much each motor should move
-            // not sure how to turn this into a useful bit for the PID controlller in this mode....
-
-
             //movement
             drive.Drive(Joy1Y.ramp(gamepad1.left_stick_y, 1.5), Joy1X.ramp(gamepad1.left_stick_x, 1.5), Joy2X.ramp(gamepad1.right_stick_x, 1.5));
 
             //lift movement
             lift.Move((double) gamepad2.left_stick_y);
-
 
             //presets for height
             if (gamepad2.x) {
@@ -70,22 +63,11 @@ public class Teleop extends LinearOpMode {
             } else {
                 lift.ClawClose();
             }
-            
+
             lift.checkOptical();
 
+            lift.testBottom();
 
-
-
-
-            /*
-            telemetry.addLine("Right Lift Sensors: ")
-                    .addData("Arm Angle: ", RLift.ReadPotentiometer())
-                    .addData("Elbow Encoder: ", RLift.ReadElbowEncoder())
-                    .addData("Elbow PID Out: ", RLift.elbowPID.getOutput())
-                    .addData("Elbow PID Act", RLift.elbowPID.isActive());
-
-            telemetry.update();
-            */
             idle();
         }
 
@@ -99,6 +81,8 @@ public class Teleop extends LinearOpMode {
                 hardwareMap.dcMotor.get("D_RL"),
                 hardwareMap.dcMotor.get("D_FL")};
 
+        drive.Initialize(init_drive);
+
         final DcMotor init_lift = hardwareMap.dcMotor.get("LIFT");
         final Servo servos[] = {
                 hardwareMap.servo.get("CLAW0"),
@@ -109,29 +93,11 @@ public class Teleop extends LinearOpMode {
         lift.Initialize(init_lift,
                 servos,
                 hardwareMap.touchSensor.get("BSTOP"),
-                hardwareMap.opticalDistanceSensor.get("C_STOP"),
+                //hardwareMap.opticalDistanceSensor.get("C_STOP"),
+                hardwareMap.get(DistanceSensor.class, "C_STOP"),
                 telemetry
         );
         // Subsystems
-        drive.Initialize(init_drive);
-        /*lLift.Initialize(hardwareMap.dcMotor.get("M_LA"),
-                hardwareMap.dcMotor.get("M_SL"),
-                hardwareMap.dcMotor.get("M_IL"),
-                hardwareMap.get(Rev2mDistanceSensor.class, "I_CD"));
-        RLift.Initialize(hardwareMap.dcMotor.get("M_RA"),
-                hardwareMap.dcMotor.get("M_RE"),
-                hardwareMap.get(CRServo.class,"CR_C"),
-                hardwareMap.get(CRServo.class,"CR_D"),
-                hardwareMap.analogInput.get("A_A"),
-                hardwareMap.get(RevTouchSensor.class,"D_CL"),
-                hardwareMap.get(RevTouchSensor.class,"D_AL"),
-                hardwareMap.get(RevTouchSensor.class, "D_EM"));
-        CSensors.Initialize(hardwareMap.get(RevColorSensorV3.class, "I_CL"),
-                hardwareMap.get(RevColorSensorV3.class, "I_CR"));
-
-        RLift.ZeroPotentiometer();
-
-         */
     }
 
     double twoButtonConvert(boolean positive, boolean negative) {
