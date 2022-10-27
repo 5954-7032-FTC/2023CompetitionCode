5fabce4d921901272f00762b88177be569fe4b53
jovanus <eric.weber.r@gmail.com>
2022-10-26 23:01:48 -0400

    further work of motion profiling.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/algorithms/motioncontrol/GenerateMotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/algorithms/motioncontrol/GenerateMotionProfile.java
@@ -1,13 +1,22 @@
 package org.firstinspires.ftc.teamcode.algorithms.motioncontrol;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class GenerateMotionProfile {
 
-    public static Queue<ProfilePoint> acceleration(ProfilePoint LastPoint, double vel, double accel, double dt){
+    /**
+     * Creates a deque that calculates and outputs the profile points required to make velocity changes
+     * @param LastPoint Provide the last point to know what the first point is
+     * @param vel Set the new velocity
+     * @param accel The acceleration of the
+     * @param dt
+     * @return
+     */
+    public static Deque<ProfilePoint> acceleration(ProfilePoint LastPoint, double vel, double accel, double dt){
 
-        Queue<ProfilePoint> output = new LinkedList<>();
+        Deque<ProfilePoint> output = new LinkedList<>();
 
         for (double t = LastPoint.getTime(); t < timetoaccelerate(LastPoint,vel,accel) ; t=+dt) {
 
@@ -21,9 +30,9 @@ public class GenerateMotionProfile {
         return output;
     }
 
-    public static Queue<ProfilePoint> cruse(ProfilePoint LastPoint, double time, double dt){
+    public static Deque<ProfilePoint> cruse(ProfilePoint LastPoint, double time, double dt){
 
-        Queue<ProfilePoint> output = new LinkedList<>();
+        Deque<ProfilePoint> output = new LinkedList<>();
 
         for (double t = LastPoint.getTime(); t < time - LastPoint.getTime() ; t=+dt) {
 
